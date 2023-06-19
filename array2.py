3faf019a591ac0b0654dcdb4d432cc3361712a7c
Devrin Anggun Saputri <136779040+DevrinAnggun@users.noreply.github.com>
2023-06-19 20:07:39 +0700

    Add files via upload

---
+++ array2.py
@@ -0,0 +1,13 @@
+buah = [["Apel", "Jeruk", "Jambu", "Anggur"]],
+["Nanas", "Melon", "Manggis", "Duku"]
+
+for i in range(len(buah)):
+    for j in range(len(buah[i])):
+        print(buah[i][j]) 
+
+n = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+for angka in n:
+    
+    if angka % 2 == 0:  
+        genap = angka
+        print(genap)
